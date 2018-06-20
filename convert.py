e799bf939f127753a105fec2919f799cba7ae504
Alexander Demidovskij <alexander.demidovskij@intel.com>
2018-06-20 10:35:50 +0300

    added more flexibility to accept jpg images

--- convert.py
+++ convert.py
@@ -46,12 +46,27 @@ def pdf2jpg(path_to_file):
 
 
 def sort_path_lambda(el):
-    return int(el.split(os.sep)[-1].split('.')[0])
+    # TODO: implement a more robust solution to make the order
+    try:
+        # expect to get /home/user/1.PDF and use 1 as a key to sort
+        res = int(el.split(os.sep)[-1].split('.')[0])
+    except ValueError:
+        # expect to get /home/user/name-1.PDF and use 1 as a key to sort
+        try:
+            res = int(el.split(os.sep)[-1].split('.')[0].split('-')[1])
+        except Exception:
+            raise ValueError('Implement your own order key')
+    return res
 
 
 PDF_ROOT = 'path_to_dir_with_pdfs'
 FINAL_PDF_NAME = 'output_name'
 NEED_ORDER = True
+ALREADY_JPG_SOURCE = False
+OFFSET_TOP = 100
+HEIGHT = 3650
+OFFSET_LEFT = 100
+WIDTH = 2700
 
 # traverse root directory, and list directories as dirs and files as files
 for root, dirs, files in os.walk(PDF_ROOT):
@@ -68,14 +83,18 @@ for root, dirs, files in os.walk(PDF_ROOT):
 
     cropped_paths = []
     for file_name in files:
-        if not file_name.upper().endswith('.PDF'):
+        if not ALREADY_JPG_SOURCE and not file_name.upper().endswith('.PDF'):
+            continue
+        elif ALREADY_JPG_SOURCE and not file_name.upper().endswith('.JPG'):
             continue
 
         print(len(path) * '---', file_name)
 
-        jpg_path = pdf2jpg(os.path.join(root, file_name))
+        jpg_path = os.path.join(root, file_name)
+        if not ALREADY_JPG_SOURCE:
+            jpg_path = pdf2jpg(os.path.join(root, file_name))
         img = cv2.imread(jpg_path)
-        crop_img = img[100:3650, 100:2700]
+        crop_img = img[OFFSET_TOP:HEIGHT, OFFSET_LEFT:WIDTH]
         rotated_cropped = imutils.rotate_bound(crop_img, 90)
 
         cropped_jpg_path = os.path.join(new_dir, file_name.replace('.PDF', '.jpg'))
@@ -83,7 +102,7 @@ for root, dirs, files in os.walk(PDF_ROOT):
         cropped_paths.append(cropped_jpg_path)
 
     with open('{}.pdf'.format(FINAL_PDF_NAME), 'wb') as f:
-        sorted_paths = sorted(cropped_paths, key=sort_path_lambda)
+        sorted_paths = sorted(cropped_paths, key=sort_path_lambda) if NEED_ORDER else cropped_paths
         f.write(img2pdf.convert([i for i in sorted_paths]))
 
     # clean up from intermediate artifacts
